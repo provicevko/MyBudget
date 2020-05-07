c837f4b2bfc2f5ba29aacb02118c1edabc9dae42
Provice <provicevk@gmail.com>
2020-05-07 23:28:15 +0300

    Create interface IBudget, class BudgetEventArgs

---
+++ BudgetLib/BugetEventArgs.cs
@@ -0,0 +1,15 @@
+﻿namespace BudgetLib
+{
+    public delegate void BudgetStateHandler(object sender, BugetEventArgs e);
+    public class BugetEventArgs
+    {
+        public string Message { get; }
+        public decimal Sum { get; }
+
+        public BugetEventArgs(string message, decimal sum)
+        {
+            Message = message;
+            Sum = sum;
+        }
+    }
+}
---
+++ BudgetLib/IBudget.cs
@@ -0,0 +1,9 @@
+﻿namespace BudgetLib
+{
+    public interface IBudget
+    {
+        void Put(decimal sum);
+        decimal Withdraw(decimal sum);
+        void Transfer(int id,decimal sum);
+    }
+}
